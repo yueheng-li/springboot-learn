ba6ccd93e54de2675d1a701d2459d8da57130e30
chunhui <syunki.ri@gmail.com>
2017-09-19 14:48:09 +0800

    Update ArticleESServiceImpl.java

--- LearnES/src/main/java/cn/java/learn/redis/service/impl/ArticleESServiceImpl.java
+++ LearnES/src/main/java/cn/java/learn/redis/service/impl/ArticleESServiceImpl.java
@@ -2,11 +2,17 @@ package cn.java.learn.redis.service.impl;
 
 import java.util.List;
 
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
+import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 
 import cn.java.learn.redis.entity.Article;
@@ -19,9 +25,12 @@ public class ArticleESServiceImpl implements ArticleESService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ArticleESServiceImpl.class);
 
     // 分页参数 -> TODO 代码可迁移到具体项目的公共 common 模块
-    private static final Integer pageNumber = 0;
-    private static final Integer pageSize = 10;
-    Pageable pageable = new PageRequest(pageNumber, pageSize);
+    private static final Integer PAGENUMBER = 0;
+    private static final Integer PAGESIZE = 10;
+
+    /* 搜索模式 */
+    String SCORE_MODE_SUM = "sum"; // 权重分求和模式
+    Float  MIN_SCORE = 10.0F;      // 由于无相关性的分值默认为 1 ，设置权重分最小值为 10
     
     @Autowired
     ArticleESRepository articeRepository;
@@ -37,5 +46,71 @@ public class ArticleESServiceImpl implements ArticleESService {
 	public List<Article> findByTitleAndAbstracts(String title, String abstracts) {
 		return articeRepository.findByTitleAndAbstracts(title, abstracts);
 	}
+
+    /**
+     * 搜索词搜索，分页返回论文信息
+     *
+     * @param pageNumber 当前页码
+     * @param pageSize 每页大小
+     * @param searchContent 搜索内容
+     * @return
+     */
+    public List<Article> searchArticle(Integer pageNumber, Integer pageSize, String searchContent) {
+
+
+        // 校验分页参数
+        if (pageSize == null || pageSize <= 0) {
+            pageSize = PAGENUMBER;
+        }
+
+        if (pageNumber == null || pageNumber < pageNumber) {
+            pageNumber = PAGESIZE;
+        }
+
+        LOGGER.info("\n searchCity: searchContent [" + searchContent + "] \n ");
+
+        // 构建搜索查询
+        SearchQuery searchQuery = getArticleSearchQuery(pageNumber,pageSize,searchContent);
+
+        LOGGER.info("\n searchCity: searchContent [" + searchContent + "] \n DSL  = \n " + searchQuery.getQuery().toString());
+
+        Page<Article> articlePage = articeRepository.search(searchQuery);
+        return articlePage.getContent();
+    }
+
+    /**
+     * 根据搜索词构造搜索查询语句
+     *
+     * 代码流程：
+     *      - 权重分查询
+     *      - 短语匹配
+     *      - 设置权重分最小值
+     *      - 设置分页参数
+     *
+     * @param pageNumber 当前页码
+     * @param pageSize 每页大小
+     * @param searchContent 搜索内容
+     * @return
+     */
+    private SearchQuery getArticleSearchQuery(Integer pageNumber, Integer pageSize,String searchContent) {
+        // 短语匹配到的搜索词，求和模式累加权重分
+        // 权重分查询 https://www.elastic.co/guide/cn/elasticsearch/guide/current/function-score-query.html
+        //   - 短语匹配 https://www.elastic.co/guide/cn/elasticsearch/guide/current/phrase-matching.html
+        //   - 字段对应权重分设置，可以优化成 enum
+        //   - 由于无相关性的分值默认为 1 ，设置权重分最小值为 10
+        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery()
+                .add(QueryBuilders.matchPhraseQuery("title", searchContent),
+                ScoreFunctionBuilders.weightFactorFunction(1000))
+                .add(QueryBuilders.matchPhraseQuery("abstracts", searchContent),
+                ScoreFunctionBuilders.weightFactorFunction(500))
+                .scoreMode(SCORE_MODE_SUM).setMinScore(MIN_SCORE);
+
+        // 分页参数
+        Pageable pageable = new PageRequest(pageNumber, pageSize);
+        return new NativeSearchQueryBuilder()
+                .withPageable(pageable)
+                .withQuery(functionScoreQueryBuilder).build();
+    }
+
 
 }

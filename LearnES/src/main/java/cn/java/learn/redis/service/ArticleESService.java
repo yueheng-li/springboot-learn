1225ec6b6134ffab3687ea5d0aef22fb6e6c3fe7
chunhui <syunki.ri@gmail.com>
2017-09-19 14:47:42 +0800

    Update ArticleESService.java

--- LearnES/src/main/java/cn/java/learn/redis/service/ArticleESService.java
+++ LearnES/src/main/java/cn/java/learn/redis/service/ArticleESService.java
@@ -3,7 +3,6 @@ package cn.java.learn.redis.service;
 import java.util.List;
 
 import cn.java.learn.redis.entity.Article;
-import cn.java.learn.redis.entity.City;
 
 public interface ArticleESService {
 
@@ -25,5 +24,15 @@ public interface ArticleESService {
      * @return
      */
     List<Article> findByTitleAndAbstracts(String title, String abstracts);
+
+    /**
+     * 搜索词搜索，分页返回论文信息
+     *
+     * @param pageNumber 当前页码
+     * @param pageSize 每页大小
+     * @param searchContent 搜索内容
+     * @return
+     */
+    List<Article> searchArticle(Integer pageNumber, Integer pageSize, String searchContent);
 
 }
